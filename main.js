b6c8b4fae3cdc7541c94c93bcdc74435a3e6a0ff
Glitch (node-chrome-page) <none>
2019-01-16 06:12:41 +0000

    👠👣 Updated with Glitch

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+exports.binaryPath = function() {
+  var puppeteer = require("puppeteer");
+  var browserFetcher = puppeteer.createBrowserFetcher();
+  var revision = process.env.PUPPETEER_CHROMIUM_REVISION || process.env.npm_config_puppeteer_chromium_revision || process.env.npm_package_config_puppeteer_chromium_revision
+    || require('puppeteer/package.json').puppeteer.chromium_revision;
+  var revisionInfo = browserFetcher.revisionInfo(revision);
+  return revisionInfo.executablePath;
+}
 exports.page = function(link) {
   return new Promise(function(resolve, reject) {
     (async function() {
@@ -5,12 +13,12 @@ exports.page = function(link) {
       var chrome = require('selenium-webdriver/chrome');
       require('chromedriver');
       let options = new chrome.Options();
-      options.setChromeBinaryPath(require("puppeteer")._launcher._projectRoot + "/.local-chromium/linux-" + require('puppeteer/package.json').puppeteer.chromium_revision + "/chrome-linux/chrome");
+      options.setChromeBinaryPath(exports.binaryPath());
       options.addArguments('--headless');
       options.addArguments('--disable-gpu');
       options.addArguments('--no-sandbox');
       options.addArguments('--window-size=1280,960');
-      const driver = await new webdriver.Builder()
+      var driver = await new webdriver.Builder()
         .forBrowser('chrome')
         .setChromeOptions(options)
         .build();
